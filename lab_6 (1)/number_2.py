62d505b068bc5483107c4dc74bcb3d8d192e32f1
student <2016.ale@inbox.ru>
2025-10-17 14:24:01 +0900

    create new file

--- lab_6 (1)/number_2.py
+++ lab_6 (1)/number_2.py
@@ -1,8 +1,31 @@
 import sys
-from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QVBoxLayout, QPushButton)
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QVBoxLayout)
 
 
 class Clicker(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Кликер")
+
+        self.count = 0
+        self.label = QLabel(f"Счёт: {self.count}")
+
+        self.button = QPushButton("Клик!")
+        self.button.clicked.connect(self.increment_count)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+
+        self.setLayout(layout)
+
+    def increment_count(self):
+        self.count += 1
+        self.label.setText(f"Счёт: {self.count}")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Clicker()
+    window.show()
+    sys.exit(app.exec())
